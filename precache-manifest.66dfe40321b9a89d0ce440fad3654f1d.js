68dc755279593f3d5ae17d896d984cf4624cd341
Maxim Razhev <svognev@yandex.ru>
2019-08-28 11:29:11 +0300

    Updates

---
+++ precache-manifest.66dfe40321b9a89d0ce440fad3654f1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61b99a9995fa613b27cdce98dbd8ecf0",
+    "url": "/newdec/index.html"
+  },
+  {
+    "revision": "86c068b1863b82031573",
+    "url": "/newdec/static/css/main.ac0bbbca.chunk.css"
+  },
+  {
+    "revision": "42563e57e4bd21903640",
+    "url": "/newdec/static/js/2.3b4ce07b.chunk.js"
+  },
+  {
+    "revision": "86c068b1863b82031573",
+    "url": "/newdec/static/js/main.9e13d314.chunk.js"
+  },
+  {
+    "revision": "6f143cbc81aa04c62ea6",
+    "url": "/newdec/static/js/runtime~main.3c986089.js"
+  }
+]);
