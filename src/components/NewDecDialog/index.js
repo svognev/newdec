3f7548ba45764a6619c9820c7e321dce407da10f
Maxim Razhev <svognev@yandex.ru>
2019-09-17 16:11:42 +0300

    change highlited options to red

--- src/components/NewDecDialog/index.js
+++ src/components/NewDecDialog/index.js
@@ -27,7 +27,7 @@ import "./style.css";
 
 class NewDecDialog extends React.Component {
     state = { 
-        openedTab: 11,
+        openedTab: 7,
         isList: true,
         listType: "unordered",
         bulletField: "",
